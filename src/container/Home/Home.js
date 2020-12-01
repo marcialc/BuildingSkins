ab4715ddf4aa37fda6dbfe9a698f2879b3673260
Marcial Cabrera <marcialcabrera@marcials-mbp.lan>
2020-11-30 19:00:35 -0500

    first commit

---
+++ src/container/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div className="Home">
+            <p id="Name">Building Skins Eng.</p>
+            <p>Website Coming Soon</p>
+        </div>
+    );
+}
+
+export default Home;
